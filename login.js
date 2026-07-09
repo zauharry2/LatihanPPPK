6bf46bdd9bdf7f02a1fed6e5cd7f5de5a797d4c5
zauharry2 <ajdkkamis@gmail.com>
2026-07-09 09:32:41 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,79 @@
+const USERNAME = "admin";
+const PASSWORD = "pppk2026";
 
+
+if(sessionStorage.getItem("loginPPPK") !== "true"){
+
+document.body.style.display="none";
+
+
+window.onload=function(){
+
+
+let loginBox = `
+
+<div id="loginBox">
+
+<h2>🔐 Login PPPK</h2>
+
+<input id="user" placeholder="Username">
+
+<input id="pass" 
+type="password"
+placeholder="Password">
+
+<button onclick="login()">
+Masuk
+</button>
+
+<p id="pesan"></p>
+
+</div>
+
+`;
+
+document.body.innerHTML = loginBox;
+
+document.body.style.display="block";
+
+}
+
+}
+
+
+
+function login(){
+
+
+let u =
+document.getElementById("user").value;
+
+
+let p =
+document.getElementById("pass").value;
+
+
+
+if(u === USERNAME && p === PASSWORD){
+
+
+sessionStorage.setItem(
+"loginPPPK",
+"true"
+);
+
+
+location.reload();
+
+
+}else{
+
+
+document.getElementById("pesan").innerHTML =
+"❌ Username atau password salah";
+
+
+}
+
+
+}
