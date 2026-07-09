05a9865c7f1830ddae8511d261874c450b3c3139
zauharry2 <ajdkkamis@gmail.com>
2026-07-09 09:45:37 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,79 +1,93 @@
-const USERNAME = "admin";
-const PASSWORD = "pppk2026";
-
-
 if(sessionStorage.getItem("loginPPPK") !== "true"){
 
-document.body.style.display="none";
+document.addEventListener("DOMContentLoaded", function(){
 
+document.body.innerHTML = `
 
-window.onload=function(){
+<div style="
+height:100vh;
+display:flex;
+justify-content:center;
+align-items:center;
+background:#2563eb;
+font-family:Arial;
+">
 
-
-let loginBox = `
-
-<div id="loginBox">
+<div style="
+background:white;
+padding:30px;
+border-radius:15px;
+width:300px;
+text-align:center;
+box-shadow:0 5px 15px #333;
+">
 
 <h2>🔐 Login PPPK</h2>
 
-<input id="user" placeholder="Username">
+<input id="username"
+placeholder="Username"
+style="width:90%;padding:10px;margin:8px">
 
-<input id="pass" 
+<br>
+
+<input id="password"
 type="password"
-placeholder="Password">
+placeholder="Password"
+style="width:90%;padding:10px;margin:8px">
 
-<button onclick="login()">
+<br>
+
+<button onclick="cekLogin()"
+style="
+background:#16a34a;
+color:white;
+border:0;
+padding:10px 30px;
+border-radius:8px;
+cursor:pointer;
+">
 Masuk
 </button>
 
-<p id="pesan"></p>
+<p id="error" style="color:red"></p>
+
+</div>
 
 </div>
 
 `;
 
-document.body.innerHTML = loginBox;
-
-document.body.style.display="block";
-
-}
+});
 
 }
 
 
 
-function login(){
+function cekLogin(){
+
+let user =
+document.getElementById("username").value;
+
+let pass =
+document.getElementById("password").value;
 
 
-let u =
-document.getElementById("user").value;
-
-
-let p =
-document.getElementById("pass").value;
-
-
-
-if(u === USERNAME && p === PASSWORD){
-
+if(user==="admin" && pass==="pppk2026"){
 
 sessionStorage.setItem(
 "loginPPPK",
 "true"
 );
 
-
 location.reload();
-
-
-}else{
-
-
-document.getElementById("pesan").innerHTML =
-"❌ Username atau password salah";
-
 
 }
 
+else{
+
+document.getElementById("error").innerHTML =
+"Username / Password salah";
+
+}
 
 }
